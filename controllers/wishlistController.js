3013aa1bb815d4bf0f8226103f6b903d3f858378
ShieldyArmor <Thegreen_dragon@hotmail.com>
2023-05-26 12:43:48 +0200

    double/tighter auth

--- controllers/wishlistController.js
+++ controllers/wishlistController.js
@@ -1,5 +1,8 @@
 // imports
 const Wishlist = require('../models/wishlist');
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const { jwtSecret } = require('../config.json');
 
 // controller
 module.exports.wishlist_create = async (req, res) => {
@@ -85,94 +88,141 @@ module.exports.wishlist_readAll = async (req, res) => {
 
 module.exports.wishlist_updateOne = async (req, res) => {
     const { username, newItem } = req.body;
+    const token = req.cookies.jwt;
+    console.log(token);
 
     // console.log(req.body);
     // console.log(username);
     // console.log(newItem);
 
-    try {
-        const dbWishlist = await Wishlist.findOne({ createdBy: username });
-        console.log(dbWishlist);
-        console.log(dbWishlist.items);
-
-
-        const newItemList = dbWishlist.items
-        
-        newItemList.push(newItem)
-        
-        console.log(newItemList);
-
-        await dbWishlist.updateOne({ items: newItemList });
-        res.status(200).send({
-            status: 'Wishlisten er oppdatert!',
-            code: 'ok'
+    if (token) {
+        jwt.verify(token, jwtSecret, async (err, decodedToken) => {
+            if (err) {
+                // ugyldig token
+                console.error(err);
+                res.cookie('jwt', '', { maxAge: 1 });
+                res.redirect('/sign-in');
+            } else {
+                // bruker er logget inn
+                try {
+                    const dbWishlist = await Wishlist.findOne({ createdBy: username });
+                    console.log(dbWishlist);
+                    console.log(dbWishlist.items);
+            
+            
+                    const newItemList = dbWishlist.items
+                    
+                    newItemList.push(newItem)
+                    
+                    console.log(newItemList);
+            
+                    await dbWishlist.updateOne({ items: newItemList });
+                    res.status(200).send({
+                        status: 'Wishlisten er oppdatert!',
+                        code: 'ok'
+                    });
+                } catch(err) {
+                    console.error(err);
+                    res.status(400).send({
+                        status: 'Wishlisten kunne desverre ikke oppdateres akkruat nå. Prøv igjen senere.',
+                        code: 'serverErr'
+                    });
+                };
+            };
         });
-    } catch(err) {
-        console.error(err);
-        res.status(400).send({
-            status: 'Wishlisten kunne desverre ikke oppdateres akkruat nå. Prøv igjen senere.',
-            code: 'serverErr'
-        });
+    } else {
+        // bruker er ikke logget inn, redirect til logg inn side
+        res.redirect('/sign-in');
     };
+
 };
 
 module.exports.wishlist_move = async (req, res) => {
     const { direction, index, username } = req.body;
+    const token = req.cookies.jwt;
+    console.log(token);
 
-    function moveItem(array, from, to) {
-        // remove `from` item and store it
-        var f = array.splice(from, 1)[0];
-        // insert stored item into position `to`
-        array.splice(to, 0, f);
-      }
+    if (token) {
+        jwt.verify(token, jwtSecret, async (err, decodedToken) => {
+            if (err) {
+                // ugyldig token
+                console.error(err);
+                res.cookie('jwt', '', { maxAge: 1 });
+                res.locals.loggedIn = false;
+                res.redirect("/sign-in");
+            } else {
+                // logget inn
+                const user = await User.findOne({ _id: decodedToken.id });
 
-    console.log(direction);
-    console.log(index);
-    console.log(username);
+                if (user) {
 
-    let newIndex = Number(index)+Number(direction);
-
-    if (newIndex < 0) {
-        res.status(400).send({
-            status: 'Cannot move first array item further up.',
-            code: 'serverErr'
+                    function moveItem(array, from, to) {
+                        // remove `from` item and store it
+                        var f = array.splice(from, 1)[0];
+                        // insert stored item into position `to`
+                        array.splice(to, 0, f);
+                      }
+                
+                    console.log(direction);
+                    console.log(index);
+                    console.log(username);
+                
+                    let newIndex = Number(index)+Number(direction);
+                
+                    if (newIndex < 0) {
+                        res.status(400).send({
+                            status: 'Cannot move first array item further up.',
+                            code: 'serverErr'
+                        });
+                    }
+                
+                    else {
+                
+                    console.log(`new Index: ${newIndex}`);
+                
+                    try { 
+                        const dbWishlist = await Wishlist.findOne({ createdBy: username });
+                        const dbItems = dbWishlist.items
+                
+                        if (dbItems.length <= newIndex) {
+                            res.status(400).send({
+                                status: 'Cannot move last array item further down.',
+                                code: 'serverErr'
+                            });
+                        }
+                        else {
+                        moveItem(dbItems, index, newIndex)
+                
+                        console.log(dbItems);
+                
+                        await dbWishlist.updateOne({ items: dbItems });
+                
+                        res.status(200).send({
+                            status: 'Wishlisten er oppdatert!',
+                            code: 'ok'
+                        });
+                        }
+                    } catch(err) {
+                        console.error(err);
+                        res.status(400).send({
+                            status: 'Error.',
+                            code: 'serverErr'
+                        });
+                    };
+                
+                }                
+                } else {
+                    // brukeren eksisterer ikke
+                    res.cookie('jwt', '', { maxAge: 1 });
+                    res.locals.loggedIn = false;
+                    res.redirect("/sign-in")
+                };
+            };
         });
-    }
-
-    else {
-
-    console.log(`new Index: ${newIndex}`);
-
-    try { 
-        const dbWishlist = await Wishlist.findOne({ createdBy: username });
-        const dbItems = dbWishlist.items
-
-        if (dbItems.length <= newIndex) {
-            res.status(400).send({
-                status: 'Cannot move last array item further down.',
-                code: 'serverErr'
-            });
-        }
-        else {
-        moveItem(dbItems, index, newIndex)
-
-        console.log(dbItems);
-
-        await dbWishlist.updateOne({ items: dbItems });
-
-        res.status(200).send({
-            status: 'Wishlisten er oppdatert!',
-            code: 'ok'
-        });
-        }
-    } catch(err) {
-        console.error(err);
-        res.status(400).send({
-            status: 'Error.',
-            code: 'serverErr'
-        });
+    } else {
+        // brukeren er ikke logget inn
+        res.locals.loggedIn = false;
+        res.redirect("/sign-in")
     };
-
-}
 
 };
